4c217683788dca07379abb6e0ef62419aaca612f
Grantallkotten <clonetroopertank@gmail.com>
2024-10-31 23:22:58 +0100

    Added more advanced player movment

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,12 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-@onready var label: Label = $HUD/HUTArea/Label
+@onready var position_label: Label = $HUD/HUDArea/PositionLabel
+@onready var velocity_label: Label = $HUD/HUDArea/VelocityLabel
 
-@onready var walk_strength: float = 1000
+@export var walk_speed: float = 10.0
+@export var walk_acceleration: float = 20.0
+@export var jump_strength: float = 5.0
+@export var gravity: float = 90.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,18 +15,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	player_movment(delta)
-	set_hud() 
+	_player_movment(delta)
+	_set_hud() 
 
-func player_movment(delta: float) -> void:
-	var input := Vector3.ZERO
+func _player_movment(delta: float) -> void:
+	var movment_direction := Vector3.ZERO
 	# Input.get_axis(A, B) --> A will return 1.0 and B -1.0 
-	input.x = Input.get_axis("ui_left", "ui_right")
-	input.z = Input.get_axis("ui_up", "ui_down")
-	input.y = Input.get_action_strength("ui_accept")
+	movment_direction.x = Input.get_axis("move_left", "move_right")
+	movment_direction.z = Input.get_axis("move_forward", "move_backwards")
+	movment_direction.y = Input.get_action_strength("jump")
+	movment_direction = movment_direction.normalized()
 	
-	apply_central_force(input * walk_strength * delta)
+	velocity.y += movment_direction.y * jump_strength
+	velocity = velocity.move_toward(movment_direction * walk_speed, walk_acceleration * delta)
 	
+	_apply_gravity(delta)
+	move_and_slide()
 	
-func set_hud() -> void:
-	label.text = str(round(position.y * 100)/100.0)
+func _apply_gravity(delta: float) -> void:
+	velocity.y -= gravity * delta
+
+func _set_hud() -> void:
+	position_label.text = str(round(position.x * 100)/100.0) + ", " + str(round(position.y * 100)/100.0) + ", " + str(round(position.z * 100)/100.0)
+	velocity_label.text = str(round(velocity.x * 100)/100.0) + ", " + str(round(velocity.y * 100)/100.0) + ", " + str(round(velocity.z * 100)/100.0)
